b860b8b196dd3b6d16779d4a7a40b1cfb2f25d01
shukruth21 <99413764+shukruth21@users.noreply.github.com>
2025-06-02 15:09:40 +0530

    Add files via upload

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Dashboard } from "./pages/dashboard"
+function App() {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+}
+
+export default App
